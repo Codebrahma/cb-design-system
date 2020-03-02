9c09ebc44315433888ac5ef8fd4e21e4020f6d7e
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2020-03-02 17:26:44 +0530

    Added IconFirst Prop to IconButton component

--- src/components/iconButton.js
+++ src/components/iconButton.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'theme-ui';
+import { Button } from 'theme-ui';
 
-const IconButton = ({ icon, children, ...otherProps }) => (
+const IconButton = ({ icon, iconFirst, children, ...otherProps }) => (
   <Button {...otherProps}>
-    {icon}
-    {children}
+    {iconFirst ? [icon, children] : [children, icon]}
   </Button>
 );
 
 IconButton.propTypes = {
   icon: PropTypes.element.isRequired,
+  iconFirst: PropTypes.bool.isRequired,
   children: PropTypes.oneOfType([
     PropTypes.element,
     PropTypes.arrayOf(PropTypes.element),
   ]).isRequired,
+};
+
+IconButton.defaultProps = {
+  iconFirst: true,
 };
 
 export default IconButton;
