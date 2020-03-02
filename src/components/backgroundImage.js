c2258a76fc4d916e9601682cd0caaeaf751e913c
asaran97 <asaran234@gmail.com>
2020-03-02 19:19:27 +0530

    removed background-content for backgroundimage

--- src/components/backgroundImage.js
+++ src/components/backgroundImage.js
@@ -2,8 +2,6 @@ import styled from '@emotion/styled';
 import { Box } from 'theme-ui';
 
 const BackgroundImage = styled(Box)`
-  background-origin: content-box;
-
   ${({ imageUrl, image, size, repeat }) => ({
     backgroundImage: imageUrl ? `url(${imageUrl})` : image,
     backgroundSize: size || 'cover',
