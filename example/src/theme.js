5945c9c0e2fcfe7a59454095c39aa29d457508d1
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2020-03-02 18:26:27 +0530

    :art: Added sample theme

---
+++ example/src/theme.js
@@ -0,0 +1,30 @@
+export default {
+  colors: {
+    text: 'white',
+    primaryLight: '#4791db',
+    primary: '#1976d2',
+    primaryDark: '#115293',
+    secondaryLight: '#e33371',
+    secondary: '#dc004e',
+    secondaryDark: '#9a0036',
+    errorLight: '#e57373',
+    error: '#f44336',
+    errorDark: '#d32f2f',
+    warningLight: '#ffb74d',
+    warning: '#ff9800',
+    warningDark: '#f57c00',
+    infoLight: '#64b5f6',
+    info: '#2196f3',
+    infoDark: '#1976d2',
+    successLight: '#81c784',
+    success: '#4caf50',
+    successDark: '#388e3c',
+    background: 'white',
+  },
+
+  buttons: {
+    primary: {
+      color: 'text',
+    }
+  }
+};
