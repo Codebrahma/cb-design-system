aabc8112c79863913fe893bcd48edfed6b76ead3
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2020-03-02 20:26:32 +0530

    Added examples for IconButton, ReadMore and Truncate

--- example/src/App.js
+++ example/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { ThemeProvider, IconButton } from 'cb-design-system'
+import { ThemeProvider, IconButton, ReadMore, Truncate } from 'cb-design-system'
+import theme from './theme';
 
 export default () => (
-  <ThemeProvider>
-    <IconButton icon={<i>Hello</i>} iconFirst={false}>Hello</IconButton>
+  <ThemeProvider theme={theme}>
+    <IconButton icon={<i>Hello</i>} iconFirst={false}>
+      Hello
+    </IconButton>
+
+    <ReadMore numberOfChars={20}>
+      Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter.
+    </ReadMore>
+
+    <Truncate>
+      Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.
+    </Truncate>
   </ThemeProvider>
 )
