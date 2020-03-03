adbda03e866be8874944caf8e2068ad7a8126fdc
asaran97 <asaran234@gmail.com>
2020-03-03 15:23:31 +0530

    changed Select component with hoc

--- src/components/select.js
+++ src/components/select.js
@@ -1,31 +1,6 @@
-import styled from '@emotion/styled';
-import {
-  Select as select,
-} from 'theme-ui';
+import { Select as select } from 'theme-ui';
+import { withPlaceHolderVariant } from '../utils/placeholderVariant';
 
-import {
-  applyVariation,
-} from './../utils/getVariations';
-
-const variant = ({
-  theme,
-  variant = 'primary',
-  themeKey = 'placeholderVariants',
-}) => applyVariation(theme, variant, themeKey);
-
-const Select = styled(select)`
-  ::-webkit-input-placeholder {
-    ${variant}
-  }
-  ::-moz-placeholder {
-    ${variant}
-  }
-  :-ms-input-placeholder {
-    ${variant}
-  }
-  :-moz-placeholder {
-    ${variant}
-  }
-`;
+const Select = withPlaceHolderVariant(select);
 
 export default Select;
