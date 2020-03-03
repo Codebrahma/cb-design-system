17dad309a2fff169b39cadc440e3c85dee637d50
Kameshwaran Sachithanantham <kamesh@codebrahma.com>
2020-03-03 12:00:10 +0530

    Added beforeAfter HOC

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export * from 'theme-ui';
 export IconButton from './iconButton';
 export Truncate from './truncate';
 export ReadMore from './readMore';
+export withBeforeAfter from './withBeforeAfter';
