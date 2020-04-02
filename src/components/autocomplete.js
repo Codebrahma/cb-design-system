9a700355aa91c3a9f43f8fababbb1ccce67f67d5
asaran97 <asaran234@gmail.com>
2020-04-02 14:33:58 +0530

    handled focusing events

--- src/components/autocomplete.js
+++ src/components/autocomplete.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import { Box, Input as input, css } from 'theme-ui';
+import { Box, Input as input, css, Flex } from 'theme-ui';
 import { Relative, Absolute } from './position';
 import { InlineBlock } from './layout';
 
@@ -12,9 +12,12 @@ import { InlineBlock } from './layout';
 // TODO: isloading
 // TODO: multi
 
+// const Icons = styled(InlineBlock)``
+
 const DropDownContainer = styled(Relative)`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   border: 1px solid #ddd;
   padding: 5px;
   border-radius: 6px;
@@ -30,7 +33,7 @@ const DropDownContainer = styled(Relative)`
   ${({ theme, focused }) =>
     css({
       borderColor: focused ? 'primary' : '',
-      borderWidth: focused ? '2px' : '',
+      borderWidth: focused ? '1px' : '',
     })(theme)}
 `;
 
@@ -59,8 +62,12 @@ const Input = styled(input)`
   border: none;
   outline: none;
   padding: 0;
-  flex-grow: 1;
+  width: 2px;
+  min-width: max-content;
 `;
+
+const Selected = Absolute;
+const Placeholder = Absolute;
 
 const ClearIcon = styled(InlineBlock)`
   padding: 0 7px;
@@ -86,8 +93,8 @@ const Autocomplete = ({
   const [isFocused, setIsFocused] = useState(false);
 
   const dropdownRef = useRef(null);
-  // const containerRef = useRef(null);
   const inputRef = useRef(null);
+  const optionsRef = useRef(options);
 
   useEffect(() => {
     visible && document.addEventListener('click', handleOutsideClick, true);
@@ -122,16 +129,23 @@ const Autocomplete = ({
   };
 
   const handleChange = ({ target: { value } }) => {
-    const newOptions = options.filter(option =>
-      option.label.toLowerCase().includes(value.toLowerCase())
-    );
+    console.log(optionsRef.current[0].label);
+    const newOptions = options.filter(option => {
+      return typeof option.label === 'string' ? option.label.toLowerCase().includes(value.toLowerCase()) : option.label.contains(value);
+      // if (typeof option.label === 'string') {
+      //   return option.label.toLowerCase().includes(value.toLowerCase());
+      // } else {
+      //   const valurRef = useRef(option.label);
+      //   console.log(valurRef.contains(value));
+      // }
+    });
     setValue(value);
     setFilteredOption(newOptions);
   };
 
   const onOptionSelect = option => {
     onChange && onChange(option);
-    setValue(option.label);
+    setValue('');
     setVisible(false);
     setSelected(option);
     setFilteredOption(null);
@@ -139,13 +153,13 @@ const Autocomplete = ({
   };
 
   const clearValue = e => {
-    // e.preventDefault();
+    e.preventDefault();
     e.stopPropagation();
-    if (value) {
-      setFilteredOption(null);
-      setSelected('');
-      setValue('');
-    }
+    // if (value) {
+    setFilteredOption(null);
+    setSelected('');
+    setValue('');
+    // }
   };
 
   const setFocus = () => {
@@ -169,28 +183,33 @@ const Autocomplete = ({
         onFocus={setFocus}
         onBlur={clearFocus}
       >
+        {!value && <Selected>{selected && selected.label}</Selected>}
+        {!value && !selected && (
+          <Placeholder>{placeholder}</Placeholder>
+        )}
         <Input
           type='text'
-          value={value !== selected.value && !visible ? selected.label : value}
+          // value={value !== selected.value && !visible ? selected.label : value}
+          value={value}
           ref={inputRef}
           onChange={handleChange}
           onClick={e => console.log('onclick')}
           name={name}
-          placeholder={placeholder}
-          defaultValue=''
           {...props}
         />
-        {isClearable && <ClearIcon onClick={clearValue}>&times;</ClearIcon>}
-        <svg
-          height='20'
-          width='20'
-          viewBox='0 0 20 20'
-          aria-hidden='true'
-          focusable='false'
-          className='css-tj5bde-Svg'
-        >
-          <path d='M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z' />
-        </svg>
+        <Flex>
+          {isClearable && <ClearIcon onClick={clearValue}>&times;</ClearIcon>}
+          <svg
+            height='20'
+            width='20'
+            viewBox='0 0 20 20'
+            aria-hidden='true'
+            focusable='false'
+            className='css-tj5bde-Svg'
+          >
+            <path d='M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z' />
+          </svg>
+        </Flex>
       </DropDownContainer>
       {Values && visible && (
         <Options>
@@ -237,6 +256,13 @@ Autocomplete.defaultProps = {
   placeholder: 'select here..',
 };
 
+const Photo = styled(Box)`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: #ddd;
+`;
+
 const A = () => {
   const options = [
     { value: 'rustic', label: 'Rustic' },
@@ -245,6 +271,21 @@ const A = () => {
     { value: 'vintage', label: 'Vintage' },
     { value: 'refurbished', label: 'Refurbished' },
   ];
+
+  const newOptions = options.map(v => {
+    const label = (
+      <Flex>
+        <Photo />
+        <span>{v.label}</span>
+      </Flex>
+    );
+
+    return {
+      label,
+      value: v.value,
+    };
+  });
+
   return <Autocomplete options={options} />;
 };
 export default A;
